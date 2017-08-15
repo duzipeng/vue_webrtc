b04dc2e5d99da056d3376e637f3dd7a2a52e6454
RegGear <june.dzp@qq.com>
2017-08-15 10:58:21 +0800

    Stream arbitrary data with RTCDataChannel

--- src/components/MyVideo.vue
+++ src/components/MyVideo.vue
@@ -1,125 +1,151 @@
 <template>
   <div class="video-div">
-    <video id="localVideo" autoplay muted ref="localVideo"></video>
-    <video id="remoteVideo" autoplay muted ref="remoteVideo"></video>
+    <textarea id="dataChannelSend" :disabled="isDisabled_dcs" placeholder="Press Start, enter some text, then press Send." ref="dataChannelSend"></textarea>
+    <textarea id="dataChannelReceive" :disabled="isDisabled_dcr" ref="dataChannelReceive"></textarea>
 
     <div>
-      <button id="startButton" ref="startButton">Start</button>
-      <button id="callButton" ref="callButton">Call</button>
-      <button id="hangupButton" ref="hangupButton">Hang Up</button>
+      <button id="startButton" :disabled="isDisabled_stb" ref="startButton">Start</button>
+      <button id="sendButton" :disabled="isDisabled_sdb" ref="sendButton">Send</button>
+      <button id="closeButton" :disabled="isDisabled_cb" ref="closeButton">Stop</button>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import JsSIP from 'jssip'
+//  import JsSIP from 'jssip'
 
   export default {
     data() {
       return {
-        localStream: {
-          type: Object
-        },
-        localPeerConnection: {
-          type: Object
-        },
-        remotePeerConnection: {
-          type: Object
-        }
+        isDisabled_dcs: {type: Boolean, default: true},
+        isDisabled_dcr: {type: Boolean, default: true},
+        isDisabled_stb: {type: Boolean, default: false},
+        isDisabled_sdb: {type: Boolean, default: false},
+        isDisabled_cb: {type: Boolean, default: false},
+        sendChannel: {type: Object},
+        receiveChannel: {type: Object},
+        localPeerConnection: {type: Object},
+        remotePeerConnection: {type: Object}
       }
     },
     mounted() {
-      this.$refs.startButton.disabled = false;
-      this.$refs.callButton.disabled = true;
-      this.$refs.hangupButton.disabled = true;
-      this.$refs.startButton.onclick = this.start;
-      this.$refs.callButton.onclick = this.call;
-      this.$refs.hangupButton.onclick = this.hangup;
+      this.isDisabled_stb = false;
+      this.isDisabled_sdb = true;
+      this.isDisabled_cb = true;
+      this.$refs.startButton.onclick = this.createConnection;
+      this.$refs.sendButton.onclick = this.sendData;
+      this.$refs.closeButton.onclick = this.closeDataChannels;
     },
     methods: {
       trace(text) {
         console.log((performance.now() / 1000).toFixed(3) + ": " + text);
       },
-      gotStream(stream) {
-        this.trace("Received local stream");
-        this.$refs.localVideo.src = URL.createObjectURL(stream);
-        this.localStream = stream;
-        this.$refs.callButton.disabled = false;
-      },
-      start() {
-        this.trace("Requesting local stream");
-        this.$refs.startButton.disabled = true;
-        navigator.getUserMedia({audio: true, video: true}, this.gotStream,
-          function (error) {
-            this.trace("getUserMedia error: " + error);
-          });
-      },
-      call() {
-        this.$refs.callButton.disabled = true;
-        this.$refs.hangupButton.disabled = false;
-        this.trace("Starting call");
-
-        if (this.localStream.getVideoTracks().length > 0) {
-          this.trace('Using video device: ' + this.localStream.getVideoTracks()[0].label);
-        }
-        if (this.localStream.getAudioTracks().length > 0) {
-          this.trace('Using audio device: ' + this.localStream.getAudioTracks()[0].label);
-        }
-
+      createConnection() {
         let servers = null;
+        this.localPeerConnection = new RTCPeerConnection(servers, {optional: [{RtcDataChannel: true}]});
+        this.trace('Created local peer connection object localPeerConnection');
 
-        this.localPeerConnection = new RTCPeerConnection(servers);
-        this.trace("Created local peer connection object localPeerConnection");
-        this.localPeerConnection.onicecandidate = this.gotLocalIceCandidate;
+        try {
+          //Reliable Data Channels not yet supported in Chrome
+          this.sendChannel = this.localPeerConnection.createDataChannel("sendDataChannel", {reliable: false});
+          this.trace('Create send data channel');
+        } catch(e) {
+          alert('Failed to create data channel. ' + 'You need Chrome M25 or later with RtcDataChannel enabled');
+          this.trace('createdDataChannel() failed with exception: ' + e.message);
+        }
+        this.localPeerConnection.onicecandidate = this.gotLocalCandidate;
+        this.sendChannel.onopen = this.handleSendChannelStateChange;
+        this.sendChannel.onclose = this.handleSendChannelStateChange;
 
-        this.remotePeerConnection = new RTCPeerConnection(servers);
-        this.trace("Created remote peer connection object remotePeerConnection");
+        this.remotePeerConnection = new RTCPeerConnection(servers, {optional:  [{RtcDataChannels: true}]});
+        this.trace('Created remote peer connection object remotePeerConnection');
         this.remotePeerConnection.onicecandidate = this.gotRemoteIceCandidate;
-        this.remotePeerConnection.onaddstream = this.gotRemoteStream;
+        this.remotePeerConnection.ondatachannel = this.gotReceiveChannel;
 
-        this.localPeerConnection.addStream(this.localStream);
-        this.trace("Added localStream to localPeerConnection");
         this.localPeerConnection.createOffer(this.gotLocalDescription, this.handleError);
+        this.isDisabled_stb = true;
+        this.isDisabled_cb = false;
       },
-      gotLocalDescription(description) {
-        this.localPeerConnection.setLocalDescription(description);
-        this.trace("Offer from localPeerConnection: \n" + description.sdp);
-        this.remotePeerConnection.setRemoteDescription(description);
-        this.remotePeerConnection.createAnswer(this.gotRemoteDescription, this.handleError);
+      sendData() {
+        let data = this.$refs.dataChannelSend.value;
+        this.sendChannel.send(data);
+        this.trace('Sent data: ' + data);
       },
-      gotRemoteDescription(description) {
-        this.remotePeerConnection.setLocalDescription(description);
-        this.trace("Answer from remotePeerConnection: \n" + description.sdp);
-        this.localPeerConnection.setRemoteDescription(description);
-      },
-      hangup() {
-        this.trace("Ending call");
+      closeDataChannels() {
+        this.trace('Closing data channels');
+        this.sendChannel.close();
+        this.trace('Closed data channel with label: ' + this.sendChannel.label);
+        this.receiveChannel.close();
+        this.trace('Closed data channel with label: ' + this.receiveChannel.label);
         this.localPeerConnection.close();
         this.remotePeerConnection.close();
         this.localPeerConnection = null;
         this.remotePeerConnection = null;
-        this.$refs.hangupButton.disabled = true;
-        this.$refs.callButton.disabled = false;
+        this.trace('Closed peer connections');
+        this.isDisabled_stb = false;
+        this.isDisabled_sdb = true;
+        this.isDisabled_cb = true;
+        this.$refs.dataChannelSend.value = "";
+        this.$refs.dataChannelReceive.value = "";
+        this.isDisabled_dcs = true;
+        this.$refs.dataChannelSend.placeholder = "Press Start, enter some text, then press Send.";
       },
-      gotRemoteStream(event) {
-        this.$refs.remoteVideo.src = URL.createObjectURL(event.stream);
-        this.trace("Received remote stream");
+      gotLocalDescription(desc) {
+        this.localPeerConnection.setLocalDescription(desc);
+        this.trace('Offer from localPeerConnection \n' + desc.sdp);
+        this.remotePeerConnection.setRemoteDescription(desc);
+        this.remotePeerConnection.createAnswer(this.gotRemoteDescription, this.handleError);
       },
-      gotLocalIceCandidate(event) {
+      gotRemoteDescription(desc) {
+        this.remotePeerConnection.setLocalDescription(desc);
+        this.trace('Answer from remotePeerConnection \n' + desc.sdp);
+        this.localPeerConnection.setRemoteDescription(desc);
+      },
+      gotLocalCandidate(event) {
+        this.trace('local ice callback');
         if (event.candidate) {
-          this.remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-          this.trace("Local ICE candidate: \n" + event.candidate.candidate);
+          this.remotePeerConnection.addIceCandidate(event.candidate);
+          this.trace('Local ICE candidate: \n' + event.candidate.candidate);
         }
       },
       gotRemoteIceCandidate(event) {
+        this.trace('remote ice callback');
         if (event.candidate) {
-          this.localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-          this.trace("Remote ICE candidate: \n " + event.candidate.candidate);
+          this.localPeerConnection.addIceCandidate(event.candidate);
+          this.trace('Remote ICE candidate: \n ' + event.candidate.candidate);
         }
       },
-      handleError() {
-
-      }
+      gotReceiveChannel(event) {
+        this.trace('Receive Channel Callback');
+        this.receiveChannel = event.channel;
+        this.receiveChannel.onmessage = this.handleMessage;
+        this.receiveChannel.onopen = this.handleReceiveChannelStateChange;
+        this.receiveChannel.onclose = this.handleReceiveChannelStateChange;
+      },
+      handleMessage(event) {
+        this.trace('Received message: ' + event.data);
+        this.$refs.dataChannelReceive.value = event.data;
+      },
+      handleSendChannelStateChange() {
+        let readyState = this.sendChannel.readyState;
+        this.trace('Send channel state is: ' + readyState);
+        if (readyState == "open") {
+          this.isDisabled_dcs = false;
+          this.$refs.dataChannelSend.focus();
+          this.$refs.dataChannelSend.placeholder = "";
+          this.isDisabled_sdb = false;
+          this.isDisabled_cb = false;
+        } else {
+          this.isDisabled_dcs = true;
+          this.isDisabled_sdb = true;
+          this.isDisabled_cb = true;
+        }
+      },
+      handleReceiveChannelStateChange() {
+        var readyState = this.receiveChannel.readyState;
+        this.trace('Receive channel state is: ' + readyState);
+      },
+      handleError(){}
     }
   }
 </script>
